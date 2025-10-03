f81f93126551cb1e6d84d8ea6b995dbbcfe0a3fc
NITIN <91000126+nitin864@users.noreply.github.com>
2025-10-03 14:09:45 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // ❎ variable = A reusable container for a value.
+        //                          A variable behaves as if it was the value it contains.
+
+        // 🟥 Primitive = simple value stored directly in memory (stack)
+        // 🟦 Reference = memory address (stack) that points to the (heap)
+
+        // 🟥 Primitive vs 🟦 Reference
+        //    ---------                      ---------
+        //    int                           string
+        //    double                    array
+        //    char                        object
+        //    boolean
+
+        int age = 21;
+        int year = 2025;
+        int quantity = 1;
+
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        String name = "Bro Code";
+        String food = "pizza";
+        String email = "fake123@gmail.com";
+        String car = "Mustang";
+        String color = "red";
+
+        System.out.println("Your choice is a " + color + " " + year + " " + car);
+        System.out.println("The price is: " + currency + price);
+
+        if(forSale){
+            System.out.println("There is a " + car + " for sale");
+        }
+        else{
+            System.out.println("The " + car + " is not for sale");
+        }
+    }
+}
