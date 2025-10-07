9df2a614f6fbebd5db0591ae1c97770456eff489
NITIN <91000126+nitin864@users.noreply.github.com>
2025-10-08 00:51:35 +0530

    Create Msin.java

---
+++ inheritence/Msin.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+class Main {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Plant plant = new Plant();
+
+        System.out.println(plant.isAlive);
+        plant.photosynthesize();
+
+    }
+
+
+}
