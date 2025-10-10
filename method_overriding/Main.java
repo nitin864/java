342f6568eea8bc40e78bbbd6eb41ae327a207769
NITIN <91000126+nitin864@users.noreply.github.com>
2025-10-10 15:27:53 +0530

    Create Main.java

---
+++ method_overriding/Main.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+class Main {
+    public static void main(String[] args) {
+          Cat cat = new Cat();
+          Dog dog = new Dog();
+          Fish fish = new Fish();
+
+          cat.move();
+          dog.move();
+          fish.move();
+    }
+
+
+}
