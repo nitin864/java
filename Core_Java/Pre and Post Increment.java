c67e8fe6e387276ce79b2108e2275da30951811e
NITIN <91000126+nitin864@users.noreply.github.com>
2025-12-30 00:43:10 +0530

    Create Pre and Post Increment.java

---
+++ Core_Java/Pre and Post Increment.java
@@ -0,0 +1,13 @@
+class Hello {
+
+    public static void main(String args[]) {
+       
+      int num1 = 3;
+      //int num2 = 4;
+
+      ++num1;  //pre-increment
+
+      //num1++ //post-increment
+      System.out.println(num1);
+    }
+}
