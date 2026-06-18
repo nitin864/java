15b9de26029066291046d05ddf9880e26cc883d6
Nitin Raj <91000126+nitin864@users.noreply.github.com>
2026-06-19 00:43:01 +0530

    Create aappend.java

---
+++ basic_concepts/aappend.java
@@ -0,0 +1,7 @@
+//implementing append method in java
+public class Main {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Heello");
+        sb.append(" World");
+        System.out.println(sb);
+    }   
