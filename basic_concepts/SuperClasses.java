e0c173fa1021e465b617a96a6ba92b61369e6b29
Nitin Raj <91000126+nitin864@users.noreply.github.com>
2026-06-18 18:49:45 +0530

    Create SuperClasses.java

---
+++ basic_concepts/SuperClasses.java
@@ -0,0 +1,21 @@
+//implentation of super classss
+
+class Animal {
+    void eat() {
+        System.out.println("Animal eats food");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();   // inherited method
+        d.bark();
+    }
+}
